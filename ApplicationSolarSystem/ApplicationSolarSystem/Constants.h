b9bdc48f2eaa60daa02f0e9f381a294bf9b89179
marija <marija.d.stankovic@elfak.rs>
2019-12-14 23:15:33 +0100

    konstante ubacene

--- ApplicationSolarSystem/ApplicationSolarSystem/Constants.h
+++ ApplicationSolarSystem/ApplicationSolarSystem/Constants.h
@@ -12,10 +12,70 @@ static const float scaleRadius = 10000.0f;
 static const float scaleHours = 1;
 
 //Objects
+#pragma region Mercury
+static const double mercury_radius = 2439.7;//the size of the planet 
+static const double mercury_orbit_radius = 58000000; //distance from the sun
+static const double mercury_self_rotatin = 1408;//hours to rotate 360 around itself, 360/self_rotatin is how many degrees it rotates around itself for 1h
+static const double mercury_Sun_rotation = 87.97; //days to rotate around the sun
+//88*24 = hours to rotate around the sun
+//360 / (88*24) = degreese in 1hour
+#pragma endregion
 
+#pragma region Venus
+static const double venus_radius = 6051.8;//
+static const double venus_orbit_radius = 108000000;
+static const double venus_self_rotatin = 5832;//hours to rotate 360 around itself, 360/self_rotatin is how many degrees it rotates around itself for 1h
+static const double venus_Sun_rotation = 224.7; //days to rotate around the sun
 
+#pragma endregion
 
+#pragma region Earth
+static const double earth_radius = 6378.1;//
+static const double earth_orbit_radius = 150000000;
+static const double earth_self_rotatin = 24;//hours to rotate 360 around itself, 360/self_rotatin is how many degrees it rotates around itself for 1h
+static const double earth_Sun_rotation = 365.26; //days to rotate around the sun
 
+#pragma endregion
+
+#pragma region Mars
+static const double mars_radius = 3396.2;// 
+static const double mars_orbit_radius = 228000000 ;
+static const double mars_self_rotatin = 25;//hours to rotate 360 around itself, 360/self_rotatin is how many degrees it rotates around itself for 1h
+static const double mars_Sun_rotation = 686.98; //days to rotate around the sun
+
+#pragma endregion
+
+#pragma region Jupiter
+static const double jupiter_radius = 71492;//
+static const double jupiter_orbit_radius = 778000000;
+static const double jupiter_self_rotatin = 10;//hours to rotate 360 around itself, 360/self_rotatin is how many degrees it rotates around itself for 1h
+static const double jupiter_Sun_rotation = 4332.82; //days to rotate around the sun
+
+#pragma endregion
+
+#pragma region Saturn
+static const double saturn_radius = 60268;// 
+static const double saturn_orbit_radius = 1427000000;
+static const double saturn_self_rotatin = 11;//hours to rotate 360 around itself, 360/self_rotatin is how many degrees it rotates around itself for 1h
+static const double saturn_Sun_rotation = 10755.7; //days to rotate around the sun
+
+#pragma endregion
+
+#pragma region Uran
+static const double uran_radius = 25559;// 
+static const double uran_orbit_radius = 2871000000;
+static const double uran_self_rotatin = 17;//hours to rotate 360 around itself, 360/self_rotatin is how many degrees it rotates around itself for 1h
+static const double uran_Sun_rotation = 30687.15; //days to rotate around the sun
+
+#pragma endregion
+
+#pragma region Neptun
+static const double neptun_radius =24764 ;//
+static const double neptun_orbit_radius = 4497000000;
+static const double neptun_self_rotatin = 16;//hours to rotate 360 around itself, 360/self_rotatin is how many degrees it rotates around itself for 1h
+static const double neptun_Sun_rotation = 60190.03; //days to rotate around the sun
+
+#pragma endregion
 
 
 #endif
